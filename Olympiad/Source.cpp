5f4da43a3ff8315db10376568b372d26b6907b23
Abdukhamitov <59801257+Abdukhamitov@users.noreply.github.com>
2020-03-08 19:39:41 +0100

    Three students, a fifth-year student, a third-year student and a freshman, live in the same dormitory room and like to participate in programming competitions according to ACM rules. Each of them has its own approach to solving problems. The fifth-year student solves all problems strictly in order - first the first, then the second, and so on to the last. A third-year student solves problems strictly in the reverse order - first last, then last but one, and so on until the first. And the first-year student first solves the simplest problem, then the simplest of the remaining tasks, and so on to the most difficult. The complexity of the task is determined by the time required to solve it. To solve the same problem, our students spend the same amount of time.

---
+++ Olympiad/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	int n, a[12];
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	cout << 1;
+	return 0;
+}
