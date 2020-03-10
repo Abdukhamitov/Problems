4d2ecf1204c3c4ad5a937f2609c1320bdda2a7a5
Abdukhamitov <59801257+Abdukhamitov@users.noreply.github.com>
2020-03-10 18:15:53 +0100

    Telephone network services are paid according to the following rule: for calls up to A minutes per month - In rubles per minute, and calls in excess of the established rate are paid at the rate of From rubles per minute. It is required to write a program that calculates the charge for using the telephone for conversations lasting T minutes per month.

---
+++ Phone/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a[3];
+	for (int i = 0; i < 4; i++)
+	{
+		cin >> a[i];
+	}
+
+	if (a[3] > a[0])
+	{
+		cout << a[0] * a[1] + ((a[3] - a[0]) * a[2]);
+	}
+	else
+	{
+		cout << a[3] * a[1];
+	}
+}
