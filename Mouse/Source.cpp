a4783895da96415d89be93cd50b87a8106455e00
Abdukhamitov <59801257+Abdukhamitov@users.noreply.github.com>
2020-03-10 16:53:50 +0100

    Once a computer mouse thought it was worth taking another rug in reserve. So that no one would notice a spare rug, the mouse decided to hide it under his rectangular rug. Sneaking into the warehouse at night, the mouse found only round rugs there. Can a mouse hide a round rug under a rectangular rug?

---
+++ Mouse/Source.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[2];
+
+	for (int i = 0; i < 3; i++)
+	{
+		cin >> a[i];
+	}
+	if (2 * a[2] <= a[0] && 2 * a[2] <= a[1])
+	{
+		cout << "YES";
+	}
+	else
+	{
+		cout << "NO";
+	}
+}
